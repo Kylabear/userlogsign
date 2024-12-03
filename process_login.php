aa37d2f2cba486ea1eb55369d99a9f487c0d0b47
Kylabear <alphabangachon@gmail.com>
2024-12-04 01:20:56 +0800

    del c

--- process_login.php
+++ process_login.php
@@ -6,10 +6,9 @@ $username = "root";
 $password = "mypasswordkylacode12A.";
 $dbname = "user_system";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
